d0661dde5374da9a0f7a6f8a8587a8bf4502d8cd
sillypears <sillypears@users.noreply.github.com>
2022-08-15 16:02:31 -0400

    Add cost

--- sort_ps6.py
+++ sort_ps6.py
@@ -6,7 +6,7 @@ import json
 import sys
 
 class PS6(object):
-    def __init__(self, day:int, time:int, dat:dt.date, entry:int, entry_unit:str, clw:str, sculpt:str, quant:int, link:str, goldbag:bool) :
+    def __init__(self, day:int, time:int, dat:dt.date, entry:int, entry_unit:str, clw:str, sculpt:str, quant:int, price:int, link:str, goldbag:bool) :
         self.day = day
         self.entry_time = time
         self.date = dat
@@ -15,6 +15,7 @@ class PS6(object):
         self.sculpt = sculpt
         self.colorway = clw
         self.quantity = quant
+        self.price = price
         self.link = link
         self.goldbag = True if goldbag == "TRUE" else False
 
@@ -29,6 +30,7 @@ class PS6(object):
             'sculpt': self.sculpt,
             'colorway': self.colorway,
             'quantity': self.quantity,
+            'price': self.price,
             'link': self.link,
             'goldbag': self.goldbag
         }
@@ -40,23 +42,44 @@ with open (sys.argv[1], 'r') as f:
     reader = csv.DictReader(f)
     headers = reader.fieldnames
     for x in reader:
-        for s in range(0, len(x['Sculpts'].split(','))):
-            # print(int(x['Date'].split('/')[2]), int(x['Date'].split('/')[0]), int(x['Date'].split('/')[1]), int(x['Time (PST)']))
-            try:
-                temp = PS6(
-                    day=int(x['Day']), 
-                    time=x['Time (PST)'], 
-                    dat=tz.localize(dt.datetime(year=int(x['Date'].split('/')[2]), month=int(x['Date'].split('/')[0]), day=int(x['Date'].split('/')[1]), hour=int(x['Time (PST)']) )).astimezone(pytz.timezone('UTC')),
-                    entry=int(x['Entry Window'].replace('m','')),
-                    entry_unit="minute",
-                    sculpt=x['Sculpts'].split(',')[s], 
-                    clw=x['Colorway'], 
-                    quant=int(x['Quantity'].split(',')[s]),
-                    link=x['Link'],
-                    goldbag=x['Gold Bag']
-                )
-                PS6Caps.append(temp)
-            except:
-                pass
+        if len(x['Colorway'].split('^')) > 1:
+            for c in range(0, len(x['Colorway'].split('^'))):
+                try:
+                    temp = PS6(
+                        day=int(x['Day']), 
+                        time=x['Time (PST)'], 
+                        dat=tz.localize(dt.datetime(year=int(x['Date'].split('/')[2]), month=int(x['Date'].split('/')[0]), day=int(x['Date'].split('/')[1]), hour=int(x['Time (PST)']) )).astimezone(pytz.timezone('UTC')),
+                        entry=int(x['Entry Window'].replace('m','')),
+                        entry_unit="minute",
+                        sculpt=x['Sculpts'], 
+                        clw=x['Colorway'].split('^')[c],
+                        quant=int(x['Quantity'].split(',')[c]),
+                        price=int(x['Price']),
+                        link=x['Link'],
+                        goldbag=x['Gold Bag']
+                    )
+                    PS6Caps.append(temp)
+                except:
+                    pass  
+        else:
+            for s in range(0, len(x['Sculpts'].split(','))):
+                # print(int(x['Date'].split('/')[2]), int(x['Date'].split('/')[0]), int(x['Date'].split('/')[1]), int(x['Time (PST)']))
+                try:
+                    temp = PS6(
+                        day=int(x['Day']), 
+                        time=x['Time (PST)'], 
+                        dat=tz.localize(dt.datetime(year=int(x['Date'].split('/')[2]), month=int(x['Date'].split('/')[0]), day=int(x['Date'].split('/')[1]), hour=int(x['Time (PST)']) )).astimezone(pytz.timezone('UTC')),
+                        entry=int(x['Entry Window'].replace('m','')),
+                        entry_unit="minute",
+                        sculpt=x['Sculpts'].split(',')[s], 
+                        clw=x['Colorway'], 
+                        quant=int(x['Quantity'].split(',')[s]),
+                        price=int(x['Price']),
+                        link=x['Link'],
+                        goldbag=x['Gold Bag']
+                    )
+                    PS6Caps.append(temp)
+                except:
+                    pass
 with open('ps6.json', 'w+') as f:
     json.dump([x.to_json() for x in PS6Caps], f, indent=4)
